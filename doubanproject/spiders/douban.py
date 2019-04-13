fd0035c77d00cf7de52869723f3df8c6e90ccaca
jeckty <zty15696436603@gmail.com>
2019-04-13 08:37:43 +0800

    提交文件

---
+++ doubanproject/spiders/douban.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from doubanproject.items import DoubanprojectItem
+from urllib import request
+
+
+class DoubanSpider(scrapy.Spider):
+    name = 'douban'
+    allowed_domains = ['www.qiushibaike.com']
+    start_urls = ['http://www.qiushibaike.com/text/']
+    basic_url= "https://www.qiushibaike.com/text/page/{}/"
+    items = []
+    page=1
+    def parse(self, response):
+        print("*"*40)
+        print(response.url)
+        div_list= response.xpath("//div[@id='content-left']/div")
+        for odiv in div_list:
+            item = DoubanprojectItem()
+            item['name']=odiv.xpath(".//div//h2/text()").extract_first().strip()
+            item['sex']=odiv.xpath(".//div/div/@class").extract_first()
+            item['age']=odiv.xpath(".//div[@class='author clearfix']//div/text()").extract_first()
+            if item['age']==None :
+                item['age']='None'
+            content=odiv.xpath(".//a[1]/div/span")
+            item['content'] = content[0].xpath("string(.)").extract_first().strip()
+            yield item
+            self.items.append(item)
+        for self.page in range(5):
+            self.page+=1
+            url=self.basic_url.format(self.page)
+
+            yield scrapy.Request(url,callback=self.parse)
+        yield self.items
+
